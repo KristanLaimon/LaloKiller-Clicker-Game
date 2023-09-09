795cc3961960f9c5907d6cde4b55294fab83b6f9
Kristan Ruíz <itan_07@hotmail.com>
2023-09-09 09:28:11 -0800

    Add project files.

---
+++ LaloKiller.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Cyan
+{
+    public class LaloKiller : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D targetSprite, crosshairSprite, backgroundSprite;
+
+        public LaloKiller()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            targetSprite = Content.Load<Texture2D>("target");
+            crosshairSprite = Content.Load<Texture2D>("crosshairs");
+            backgroundSprite = Content.Load<Texture2D>("sky");
+
+            // use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Add your drawing code here
+
+            _spriteBatch.Begin();
+            Vector2 position = new Vector2(100, 100);
+            Vector2 size = new Vector2(100, 110);
+            _spriteBatch.Draw(backgroundSprite, new Vector2(0,0), Color.White) ;
+            _spriteBatch.Draw(targetSprite, new Rectangle((int)position.X,(int)position.Y, (int)size.X, (int)size.Y), Color.White) ;
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Cyan.LaloKiller();
+game.Run();
